e9f7ce163f3bb686786476ce87b11f7814e8d25f
Angela J Walker <ajw0026@tux207.eng.auburn.edu>
2015-04-04 17:10:43 -0500

    Added Pipeline.

---
+++ project2/server/Packet.java
@@ -0,0 +1,39 @@
+public class Packet 
+{
+	//Class variables
+	boolean ACK;
+	boolean NAK;	
+	int sequenceNumber;
+	final int PACKET_SIZE = 512;
+	byte[] payload = new byte[PACKET_SIZE];
+	
+	
+	public Packet(int sequenceNumberIn, byte[] payloadIn)
+	{
+		this.ACK = false;
+		this.NAK = false;
+		this.sequenceNumber = sequenceNumberIn;
+		this.payload = payloadIn;
+	}
+
+	public void setACK()
+	{
+		this.ACK = true;
+	}
+
+	public void setNAK()
+	{
+		this.NAK = true;
+	}
+
+	public boolean getACK()
+	{
+		return this.ACK;
+	}
+
+	public boolean getNAK()
+	{
+		return this.NAK;
+	}
+
+}
